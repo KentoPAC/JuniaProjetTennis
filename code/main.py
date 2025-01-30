21cf19d73aabc071d15a90b1dde24c7417b5b196
Destombes Yvi <99027395+yvi4410@users.noreply.github.com>
2025-01-30 13:31:29 +0100

    sans commentaire main.py

--- code/main.py
+++ code/main.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO
 import os
 import glob
 
-# Chemin du dossier de sauvegarde
+
 output_dir = "../assets/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Supprimer les anciens fichiers d'images
+
 files_to_delete = glob.glob(os.path.join(output_dir, "detection_*.png"))
 for file in files_to_delete:
     try:
@@ -16,11 +16,11 @@ for file in files_to_delete:
     except Exception as e:
         print(f"Erreur lors de la suppression de {file} : {e}")
 
-# Initialisation du modèle YOLO
+
 model_path = "../code/best.pt"
 model = YOLO(model_path)
 
-# Activer la capture vidéo avec des options supplémentaires
+
 cap = cv2.VideoCapture("udp://127.0.0.1:5000?fifo_size=1000000&overrun_nonfatal=1", cv2.CAP_FFMPEG)
 if not cap.isOpened():
     print("Erreur : Impossible d'accéder à la caméra.")
@@ -37,10 +37,10 @@ try:
             break
 
         try:
-            # Détection YOLO
+            
             results = model.predict(frame, conf=0.4, iou=0.3, verbose=False)
 
-            # Dessiner les résultats sur la frame
+            
             if results and results[0].boxes:
                 for box in results[0].boxes:
                     x, y, width, height = (
@@ -65,11 +65,11 @@ try:
                         2,
                     )
 
-                # Sauvegarder les images
+                
                 output_image_path = os.path.join(output_dir, f"detection_{frame_counter}.png")
                 cv2.imwrite(output_image_path, frame)
 
-            # Afficher la frame
+            
             cv2.imshow("Détection en direct", frame)
 
         except Exception as e:
@@ -77,7 +77,7 @@ try:
 
         frame_counter += 1
 
-        # Quitter si l'utilisateur appuie sur 'q'
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
