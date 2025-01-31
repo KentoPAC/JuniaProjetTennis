3803212fe105f802d200793e86886522ca3ebfd1
KentoPAC <pacaukento14@gmail.com>
2025-01-31 11:33:00 +0100

    rectification dans le code

--- code/infer_in_video.py
+++ code/infer_in_video.py
@@ -64,36 +64,6 @@ if __name__ == '__main__':
     
     frames, fps = read_video(args.input_path)
     frames_upd = []
-    for image in tqdm(frames):
-        img = cv2.resize(image, (OUTPUT_WIDTH, OUTPUT_HEIGHT))
-        inp = (img.astype(np.float32) / 255.)
-        inp = torch.tensor(np.rollaxis(inp, 2, 0))
-        inp = inp.unsqueeze(0)
-
-        out = model(inp.float().to(device))[0]
-        pred = F.sigmoid(out).detach().cpu().numpy()
-
-        points = []
-        for kps_num in range(14):
-            heatmap = (pred[kps_num] * 255).astype(np.uint8)
-            x_pred, y_pred = postprocess(heatmap, low_thresh=170, max_radius=25)
-            if args.use_refine_kps and kps_num not in [8, 12, 9] and x_pred and y_pred:
-                x_pred, y_pred = refine_kps(image, int(y_pred), int(x_pred))
-            points.append((x_pred, y_pred))
-
-        if args.use_homography:
-            matrix_trans = get_trans_matrix(points)
-            if matrix_trans is not None:
-                points = cv2.perspectiveTransform(refer_kps, matrix_trans)
-                points = [np.squeeze(x) for x in points]
-
-        for j in range(len(points)):
-            if points[j][0] is not None:
-                image = cv2.circle(image, (int(points[j][0]), int(points[j][1])),
-                                  radius=0, color=(0, 0, 255), thickness=10)
-                cv2.putText(image, f"Point {j}", (int(points[j][0]) + 10, int(points[j][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-        frames_upd.append(image)
-
     # Créer une liste pour stocker les données des points
     frames_points = []
 
