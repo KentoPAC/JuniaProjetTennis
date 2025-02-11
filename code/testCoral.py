0346de9b5d7b17413895534a10038ff3238942f8
Naela <strotz.naela@gmail.com>
2025-02-11 16:57:48 +0100

    modif

--- code/testCoral.py
+++ code/testCoral.py
@@ -3,27 +3,25 @@ from edgetpu.classification.engine import ClassificationEngine
 from edgetpu.utils import dataset_utils
 from PIL import Image
 
-
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--model', help='File path of Tflite model.', required=True)
-  parser.add_argument('--label', help='File path of label file.', required=True)
-  parser.add_argument(
-      '--image', help='File path of the image to be recognized.', required=True)
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model', help='File path of Tflite model.', required=True)
+    parser.add_argument('--label', help='File path of label file.', required=True)
+    parser.add_argument(
+        '--image', help='File path of the image to be recognized.', required=True)
+    args = parser.parse_args()
 
-  # Prepare labels.
-  labels = dataset_utils.read_label_file(args.label)
-  # Initialize engine.
-  engine = ClassificationEngine(args.model)
-  # Run inference.
-  img = Image.open(args.image)
-  for result in engine.classify_with_image(img, top_k=3):
-    print('---------------------------')
-    print(labels[result[0]])
-    print('Score : ', result[1])
+    # Prepare labels.
+    labels = dataset_utils.read_label_file(args.label)
+    # Initialize engine.
+    engine = ClassificationEngine(args.model)
+    # Run inference.
+    img = Image.open(args.image)
+    for result in engine.classify_with_image(img, top_k=3):
+        print('---------------------------')
+        print(labels[result[0]])
+        print('Score : ', result[1])
 
-
-if name == 'main':
-  main()
+if __name__ == '__main__':
+    main()
