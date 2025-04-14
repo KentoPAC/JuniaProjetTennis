e30aa78fadf5f27d61986cd6241b85e0fdc2aa5f
KentoPAC <pacaukento14@gmail.com>
2025-04-14 09:41:43 +0200

    AJout du code pour limiter le nombre de secondes traité

--- code/terrain/infer_in_video.py
+++ code/terrain/infer_in_video.py
@@ -11,8 +11,12 @@ import argparse
 import json
 from scipy.stats import mode
 
+# Durée souhaitée en secondes pour la détection
+detection_duration = 5  # par exemple 5 secondes
+
+
 def read_video(path_video):
-    """ Read video file """
+    """Read video file"""
     cap = cv2.VideoCapture(path_video)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     frames = []
@@ -25,28 +29,40 @@ def read_video(path_video):
     cap.release()
     return frames, fps
 
+
 def write_video(imgs_new, fps, path_output_video):
     if not imgs_new:
         print("⚠ Erreur : Aucune frame à écrire dans la vidéo finale !")
         return
     height, width = imgs_new[0].shape[:2]
-    out = cv2.VideoWriter(path_output_video, cv2.VideoWriter_fourcc(*'DIVX'), fps, (width, height))
+    out = cv2.VideoWriter(
+        path_output_video, cv2.VideoWriter_fourcc(*"DIVX"), fps, (width, height)
+    )
     for frame in imgs_new:
         out.write(frame)
     out.release()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, help='path to model')
-    parser.add_argument('--input_path', type=str, help='path to input video')
-    parser.add_argument('--output_path', type=str, help='path to output video')
-    parser.add_argument('--use_refine_kps', action='store_true', help='whether to use refine kps postprocessing')
-    parser.add_argument('--use_homography', action='store_true', help='whether to use homography postprocessing')
+    parser.add_argument("--model_path", type=str, help="path to model")
+    parser.add_argument("--input_path", type=str, help="path to input video")
+    parser.add_argument("--output_path", type=str, help="path to output video")
+    parser.add_argument(
+        "--use_refine_kps",
+        action="store_true",
+        help="whether to use refine kps postprocessing",
+    )
+    parser.add_argument(
+        "--use_homography",
+        action="store_true",
+        help="whether to use homography postprocessing",
+    )
     args = parser.parse_args()
 
     model = BallTrackerNet(out_channels=15)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model = model.to(device)
     model.load_state_dict(torch.load(args.model_path, map_location=device))
     model.eval()
@@ -55,13 +71,20 @@ if __name__ == '__main__':
 
     OUTPUT_WIDTH = 640
     OUTPUT_HEIGHT = 360
-    
+
     frames, fps = read_video(args.input_path)
     all_points = []
 
-    for image in tqdm(frames):
+    # Calcul du nombre de frames à traiter
+    num_detection_frames = int(fps * detection_duration)
+
+    # Limiter la boucle aux frames correspondant à detection_duration secondes
+    for image in tqdm(frames[:num_detection_frames]):
+        # Traitement de l'image...
         img = cv2.resize(image, (OUTPUT_WIDTH, OUTPUT_HEIGHT))
-        inp = torch.tensor(np.rollaxis(img.astype(np.float32) / 255., 2, 0)).unsqueeze(0)
+        inp = torch.tensor(np.rollaxis(img.astype(np.float32) / 255.0, 2, 0)).unsqueeze(
+            0
+        )
 
         out = model(inp.float().to(device))[0]
         pred = F.sigmoid(out).detach().cpu().numpy()
@@ -78,7 +101,9 @@ if __name__ == '__main__':
         if args.use_homography:
             matrix_trans = get_trans_matrix(points)
             if matrix_trans is not None:
-                points_np = np.array([p for p in points if p is not None], dtype=np.float32).reshape(-1, 1, 2)
+                points_np = np.array(
+                    [p for p in points if p is not None], dtype=np.float32
+                ).reshape(-1, 1, 2)
                 points_transformed = cv2.perspectiveTransform(points_np, matrix_trans)
                 points = [tuple(np.squeeze(x)) for x in points_transformed]
 
@@ -86,7 +111,7 @@ if __name__ == '__main__':
 
     # Convertir en tableau NumPy
     all_points = np.array(all_points, dtype=object)
-    num_points = 14  
+    num_points = 14
 
     # Calculer le mode (point le plus fréquent)
     most_frequent_points = []
@@ -103,20 +128,34 @@ if __name__ == '__main__':
         else:
             most_frequent_points.append(None)
 
-
     # Appliquer une homographie sur les points moyens
     if args.use_homography:
-        avg_points_np = np.array([p for p in most_frequent_points if p is not None], dtype=np.float32).reshape(-1, 1, 2)
+        avg_points_np = np.array(
+            [p for p in most_frequent_points if p is not None], dtype=np.float32
+        ).reshape(-1, 1, 2)
         matrix_trans = get_trans_matrix(most_frequent_points)
         if matrix_trans is not None:
-            avg_points_transformed = cv2.perspectiveTransform(avg_points_np, matrix_trans)
-            avg_points_transformed = [tuple(np.squeeze(x)) for x in avg_points_transformed]
-            most_frequent_points[:len(avg_points_transformed)] = avg_points_transformed
+            avg_points_transformed = cv2.perspectiveTransform(
+                avg_points_np, matrix_trans
+            )
+            avg_points_transformed = [
+                tuple(np.squeeze(x)) for x in avg_points_transformed
+            ]
+            most_frequent_points[: len(avg_points_transformed)] = avg_points_transformed
 
     # Sauvegarde des points moyens dans un fichier JSON
     json_filename = "most_frequent_points.json"
     with open(json_filename, "w") as json_file:
-        json.dump({"points": [({"x": p[0], "y": p[1]} if p else None) for p in most_frequent_points]}, json_file, indent=4)
+        json.dump(
+            {
+                "points": [
+                    ({"x": p[0], "y": p[1]} if p else None)
+                    for p in most_frequent_points
+                ]
+            },
+            json_file,
+            indent=4,
+        )
 
     print(f"Points frequent sauvegardés dans {json_filename}")
 
@@ -125,9 +164,23 @@ if __name__ == '__main__':
     for image in frames:
         for j, point in enumerate(most_frequent_points):
             if point is not None:
-                image = cv2.circle(image, (int(point[0]), int(point[1])), radius=1, color=(0, 255, 0), thickness=2)
-                cv2.putText(image, f"Point {j}", (int(point[0]) + 10, int(point[1]) - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                image = cv2.circle(
+                    image,
+                    (int(point[0]), int(point[1])),
+                    radius=1,
+                    color=(0, 255, 0),
+                    thickness=2,
+                )
+                cv2.putText(
+                    image,
+                    f"Point {j}",
+                    (int(point[0]) + 10, int(point[1]) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
         frames_upd.append(image)
 
     write_video(frames_upd, fps, args.output_path)
